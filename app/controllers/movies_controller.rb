ccb3f1c91b889b56046812d18848f5087922c7fc
StefanFisher <smfisheruga@gmail.com>
2013-11-29 01:46:16 -0500

    add movie view show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,8 +6,25 @@ class MoviesController < ApplicationController
   end
 
   def create
-  	
-  	render text: params[:movie].inspect
+
+  	#render text: params[:movie].inspect
+  	@movie = Movie.new(movie_params)
+  	@movie.save
+  	redirect_to @movie
+
+  end
+
+  def show
+  	#instance variables get passed to the new
+  	@movie = Movie.find(params[:id])
+
+  end
+
+  private
+
+  def movie_params
+
+  	params.require(:movie).permit(:title,:text)
 
   end
 
