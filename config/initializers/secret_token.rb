beba37442f25737823900929f9bf445b2ba7f757
StefanFisher <smfisheruga@gmail.com>
2013-11-28 22:48:23 -0500

    Created Lazy Day Planner

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lazydayplanner::Application.config.secret_key_base = 'c8de162f31f3f7d487960ec473b38152095550c7256794dd68525e692c6cca5889889494e164e927e7c298b85caa5ee98fc632c3ea5422f48c591aa1f56bb464'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lazydayplanner::Application.config.session_store :cookie_store, key: '_lazydayplanner_session'
