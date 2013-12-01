16d0312d8599a3ef8a328641104f52060496bb27
StefanFisher <smfisheruga@gmail.com>
2013-12-01 16:01:36 -0500

    added destroy methods for comments and dependecy to movie destruction

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,12 @@ def create
     redirect_to movie_path(@movie)
   end
 
+def destroy
+    @movie = Movie.find(params[:movie_id])
+    @comment = @movie.comments.find(params[:id])
+    @comment.destroy
+    redirect_to movie_path(@movie)
+  end
+ 
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-
-	has_many :comments
+	#movies have many comments (bulds the join) and comments are also destroyed when movies are destroyed
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true,length: {minimum: 2}
 end
