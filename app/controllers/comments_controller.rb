de74932c28d9a50d54ffde760a659b30e8efe3d1
StefanFisher <smfisheruga@gmail.com>
2013-12-01 11:56:02 -0500

    create comment controller and view. Add comments to Movie show view

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+class CommentsController < ApplicationController
+
+def create
+    @movie = Movie.find(params[:movie_id])
+    @comment = @movie.comments.create(params[:comment].permit(:commenter, :body))
+    redirect_to movie_path(@movie)
+  end
+
+end
