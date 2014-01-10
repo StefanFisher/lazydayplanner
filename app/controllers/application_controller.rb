de3a8e5fcacfcfe29dca9b56946baff9620700f3
StefanFisher <smfisheruga@gmail.com>
2014-01-09 19:15:43 -0500

    finish with devise model set up.Users appear to be working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #force user to be logged in
+  before_action :authenticate_user!
 end
