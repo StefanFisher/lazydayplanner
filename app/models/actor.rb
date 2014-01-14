1e96f2a8c67fe051a784bf125e97f0f6675f1ced
StefanFisher <smfisheruga@gmail.com>
2014-01-13 23:17:19 -0500

    Adjust method that sets actor's names to account for multi word last names like Mc Donald or the suffix Jr

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,7 @@ class Actor < ActiveRecord::Base
 	private
 
 		def fill_out_name
+			skips = ["de","le","la","mc","mac", "von", "van"]
 			arr = self.full.split(' ')
 			case(self.full.count(' '))
 				when 1
@@ -14,12 +15,25 @@ class Actor < ActiveRecord::Base
 					self.last_name= arr[1]
 				when 2
 					self.first_name=arr[0]
-					self.middle_name=arr[1]
-					self.last_name=arr[2]
+					skips.each do |x|
+						if arr[1] != x
+							self.middle_name=arr[1]
+							self.last_name=arr[2]
+						else
+							self.last_name = arr[1]+" "+arr[2]
+						end
+					end
 				when 3
 					self.first_name=arr[0]
-					self.middle_name=arr[1]+" "+arr[2]
-					self.last_name=arr[3]
+					skips.each do |x|
+						if arr[2] != x && arr[3] != "Jr."
+							self.middle_name=arr[1]+" "+arr[2]
+							self.last_name=arr[3]
+						else
+							self.middle_name=arr[1]
+							self.last_name = arr[2]+" "+arr[3]
+						end
+					end
 			end
 		end	
 end
